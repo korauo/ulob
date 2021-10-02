0494a2ab3d5b8d444ef68701f5f9d0b34d8f7d0e
SpreeHertz <spreehertzofficial@gmail.com>
2021-10-02 18:34:13 +0530

    ESLint

--- commands/helpinfo.js
+++ commands/helpinfo.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const pagination = require('discord.js-pagination');
 const Discord = require('discord.js');
 
@@ -9,44 +10,44 @@ module.exports = {
      * @param {Message} message
      * @param {String[]} args
      */
-    async run (bot, message, args) {
+	async run(bot, message, args) {
 
-        const BotInfo = new Discord.MessageEmbed()
-        .setColor(0x9EC1CF)
-        .setTitle('Bot Information')
-        .addField('**Prefix**', 'Bots prefix is: `!`')
-        .addField('**Pages**', '`1. Bot Information`, `2. Moderation`, `3. Information`')
-        .addField('**Navigation Help**', 'Use the arrows below to look through the pages!')
+		const BotInfo = new Discord.MessageEmbed()
+			.setColor(0x9EC1CF)
+			.setTitle('Bot Information')
+			.addField('**Prefix**', 'Bots prefix is: `!`')
+			.addField('**Pages**', '`1. Bot Information`, `2. Moderation`, `3. Information`')
+			.addField('**Navigation Help**', 'Use the arrows below to look through the pages!');
 
-        const Moderation = new Discord.MessageEmbed()
-        .setColor(0xFF6663)
-        .setTitle('Moderation')
-        .addField('`!ban`', 'Ban a member.')
-        .addField('`!unban`', 'Unban a member.')
-        .addField('`!kick`', 'Kick a member.')
-        .addField('`!warn`', 'Warn a member.')
-        .addField('`!mute`', 'Mute a member so they cannot type or speak.')
-        .addField('`!purge`', 'Delete a number of messages from a channel.')
-        .addField('`!lock`', 'Lock a channel with optional timer and message.')
-        .addField('`!lockend`', 'End lockdown of a channel')
+		const Moderation = new Discord.MessageEmbed()
+			.setColor(0xFF6663)
+			.setTitle('Moderation')
+			.addField('`!ban`', 'Ban a member.')
+			.addField('`!unban`', 'Unban a member.')
+			.addField('`!kick`', 'Kick a member.')
+			.addField('`!warn`', 'Warn a member.')
+			.addField('`!mute`', 'Mute a member so they cannot type or speak.')
+			.addField('`!purge`', 'Delete a number of messages from a channel.')
+			.addField('`!lock`', 'Lock a channel with optional timer and message.')
+			.addField('`!lockend`', 'End lockdown of a channel');
 
-        const Information = new Discord.MessageEmbed()
-        .setColor(0xFDFD97)
-        .setTitle('Information')
-        .addField('`!ping`', 'Ask it a question and it will respond.....')
-        .addField('`!userinfo`', 'Tells you the info of an user')
-        .addField('`!serverinfo`', 'Tells you the info of a server')
+		const Information = new Discord.MessageEmbed()
+			.setColor(0xFDFD97)
+			.setTitle('Information')
+			.addField('`!ping`', 'Ask it a question and it will respond.....')
+			.addField('`!userinfo`', 'Tells you the info of an user')
+			.addField('`!serverinfo`', 'Tells you the info of a server');
 
-        const pages = [
-            BotInfo,
-            Moderation,
-            Information,
-        ]
+		const pages = [
+			BotInfo,
+			Moderation,
+			Information,
+		];
 
-        const emojiList = ["⏪", "⏩"]
+		const emojiList = ["⏪", "⏩"];
 
-        const timeout = '600000';
+		const timeout = '600000';
 
-        pagination(message, pages, emojiList, timeout)
-    }
-}
+		pagination(message, pages, emojiList, timeout);
+	},
+};
--- commands/kick.js
+++ commands/kick.js
@@ -1,40 +1,43 @@
-const Discord = require('discord.js')
+/* eslint-disable no-empty */
+const Discord = require('discord.js');
 
 module.exports = {
-    name: "kick",
-    description: "kick command",
+	name: "kick",
+	description: "kick command",
 
-    async run (bot, message, args) {
-        if (!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("You cant use this command!")
+	async run(bot, message, args) {
+		if (!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("You cant use this command!");
 
-        const mentionMember = message.mentions.members.first();
-        let reason = args.slice(1).join(" ");
-        if (!reason) reason = "No reason given";
+		const mentionMember = message.mentions.members.first();
+		let reason = args.slice(1).join(" ");
+		if (!reason) reason = "No reason given";
 
-        const kickembed = new Discord.MessageEmbed()
-        .setTitle(`You were kicked from **${message.guild.name}**`)
-        .setDescription(`Reason: ${reason}`)
-        .setColor("RANDOM")
-        .setTimestamp()
-        .setFooter(bot.user.tag, bot.user.displayAvatarURL())
+		const kickembed = new Discord.MessageEmbed()
+			.setTitle(`You were kicked from **${message.guild.name}**`)
+			.setDescription(`Reason: ${reason}`)
+			.setColor("RANDOM")
+			.setTimestamp()
+			.setFooter(bot.user.tag, bot.user.displayAvatarURL());
 
-        if (!args[0]) return message.channel.send("You need to specify a user to kick");
+		if (!args[0]) return message.channel.send("You need to specify a user to kick");
 
-        if(!mentionMember) return message.channel.send("This user is not a valid user / is no-longer in the server!");
+		if (!mentionMember) return message.channel.send("This user is not a valid user / is no-longer in the server!");
 
-        if(!mentionMember.kickable) return message.channel.send("I was unable to kick this user!");
+		if (!mentionMember.kickable) return message.channel.send("I was unable to kick this user!");
 
 
-        try {
-            await mentionMember.send(kickembed);
-        } catch (err) {
+		try {
+			await mentionMember.send(kickembed);
+		}
+		catch (err) {
 
-        }
+		}
 
-        try {
-            await mentionMember.kick(reason);
-        } catch (err) {
-            return message.channel.send("I was unabe to kick this user! Sorry...")
-        }
-    }
-}
+		try {
+			await mentionMember.kick(reason);
+		}
+		catch (err) {
+			return message.channel.send("I was unabe to kick this user! Sorry...");
+		}
+	},
+};
