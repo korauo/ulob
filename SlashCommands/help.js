2f82964f1646d51c8904d07a7309805d8cc6990d
SpreeHertz <spreehertzofficial@gmail.com>
2021-10-02 18:28:41 +0530

    initial commit

---
+++ SlashCommands/help.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+const { Message, Client, MessageEmbed } = require("discord.js");
+
+module.exports = {
+	name: "help",
+	/**
+     *
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+	run: async (client, interaction) => {
+		const newEmbed = new MessageEmbed()
+			.setColor('#CCA383')
+			.setTitle('Texture Pack Commands')
+			.setURL('https://dsc.gg/packs')
+			.setDescription('bruh')
+			.setImage('https://cdn.discordapp.com/avatars/893037327440568371/e2fce81f5b26bc463a0f9d5f46cb05d4.png?size=80');
+		interaction.followUp({ embeds: [newEmbed] });
+	},
+};
---
+++ SlashCommands/ping.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+const { Message, Client, MessageEmbed } = require("discord.js");
+
+module.exports = {
+	name: "ping",
+	/**
+     *
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+	run: async (client, interaction) => {
+		interaction.followUp({ content: `Pong! ${client.ws.ping}ms`, ephemeral: true });
+	},
+};
