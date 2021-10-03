d0610f4ffaab43fddc66110883ceaa8236d6fd38
SpreeHertz <spreehertzofficial@gmail.com>
2021-10-03 14:25:13 +0530

    update ban.js

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -4,25 +4,25 @@ module.exports = {
 	name: "ban",
 	description: "ban command",
 
-	async run(bot, message, args) {
-		if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You cant use this command!");
+	async run(client, message, args) {
+		if (!message.member.permissions.has("BAN_MEMBERS")) return message.channel.send("You cant use this command!");
 
 		const mentionMember = message.mentions.members.first();
 		let reason = args.slice(1).join(" ");
-		if (!reason) reason = "No reason given";
+		if (!reason) reason = "no reason";
 
 		const embed = new Discord.MessageEmbed()
 			.setTitle(`You were banned from **${message.guild.name}**`)
 			.setDescription(`Reason: ${reason}`)
 			.setColor("RANDOM")
 			.setTimestamp()
-			.setFooter(bot.user.tag, bot.user.displayAvatarURL());
+			.setFooter(client.user.tag, client.user.displayAvatarURL());
 
-		if (!args[0]) return message.channel.send("You need to specify a user to ban");
+		if (!args[0]) return message.channel.send("You need to specify a user to ban.");
 
-		if (!mentionMember) return message.channel.send("This user is not a valid user / is no-longer in the server!");
+		if (!mentionMember) return message.channel.send("This user is not a valid user / is no-longer in the server.");
 
-		if (!mentionMember.bannable) return message.channel.send("I was unable to ban this user!");
+		if (!mentionMember.bannable) return message.channel.send("I was unable to ban this user.");
 
 		await mentionMember.send(embed);
 		await mentionMember.ban({
