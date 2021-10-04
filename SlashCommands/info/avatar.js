3f78e5617251daa6a7e8211676463967773aafda
SpreeHertz <spreehertzofficial@gmail.com>
2021-10-04 13:40:42 +0530

    Follow ESLint rules for avatar.js

--- SlashCommands/info/avatar.js
+++ SlashCommands/info/avatar.js
@@ -1,28 +1,28 @@
-const { Client, MessageEmbed, User, Interaction } = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-    name: 'avatar',
-    description: 'Get the Avatar of the mentioned member',
-    options: [{
-        name: 'target',
-        type: 'USER',
-        description: 'Select a user',
-        required: true,
-    }],
-    run: async (client, interaction, options) => {
-        const user = interaction.options.getUser('target')
+	name: 'avatar',
+	description: 'Get the Avatar of the mentioned member',
+	options: [{
+		name: 'target',
+		type: 'USER',
+		description: 'Select a user',
+		required: true,
+	}],
+	run: async (client, interaction) => {
+		const user = interaction.options.getUser('target');
 
-        const embed = new MessageEmbed()
-            .setTitle(`${user.username}'s Avatar`)
-            .setColor('#2F3136')
-            .setImage(user.displayAvatarURL({
-                dynamic: true,
-                size: 1024
-            }))
-            .setFooter(`Requested by ${interaction.user.username}`, interaction.user.displayAvatarURL({ dynamic: true }));
-        
-        await interaction.followUp({
-            embeds: [embed]
-        }); 
-    }
-}
+		const embed = new MessageEmbed()
+			.setTitle(`${user.username}'s Avatar`)
+			.setColor('#2F3136')
+			.setImage(user.displayAvatarURL({
+				dynamic: true,
+				size: 1024,
+			}))
+			.setFooter(`Requested by ${interaction.user.username}`, interaction.user.displayAvatarURL({ dynamic: true }));
+
+		await interaction.followUp({
+			embeds: [embed],
+		});
+	},
+};
