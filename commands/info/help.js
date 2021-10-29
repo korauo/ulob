ee3403f727b2f5de00ad5a33bd0ee7c8cd1a6348
SpreeHertz <spreehertzofficial@gmail.com>
2021-10-29 22:19:55 +0530

    Fix help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -11,17 +11,18 @@ module.exports = {
      */
 	run: async (client, message, args) => {
 		if (!args[0]) {
-			const embed1 = new MessageEmbed()
-			    .setTitle('ulob')
+			// no args
+			const noArgs = new MessageEmbed()
+				.setTitle('ulob')
 				.setColor('#41A2D5')
-				.addField('Moderation', '\`\`\`u!help moderation\`\`\`', true)
-				.addField('Information', '\`\`\`u!help information\`\`\`', true)
-				.addField('Fun', '\`\`\`u!help fun\`\`\`', true)
-
-			return message.channel.send({ embeds: [embed1] });
+				.addField('Moderation', '```u!help moderation```', true)
+				.addField('Information', '```u!help information```', true)
+				.addField('Fun', '```u!help fun```', true);
+			return message.channel.send({ embeds: [noArgs] });
 		}
-		if (args[0] === 'mod' || 'moderation') {
-			const embed2 = new MessageEmbed()
+		// args = mod
+		else if (args[0] === 'mod') {
+			const modEmbed = new MessageEmbed()
 				.setColor(0xFF6663)
 				.setTitle('<:534908853201:894270297723797554> Moderation')
 				.addField('`u!ban`', 'Ban a member.')
@@ -31,21 +32,42 @@ module.exports = {
 				.addField('`u!purge`', 'Delete a number of messages from a channel.')
 				.addField('`u!lock`', 'Lock a channel with optional timer and message.')
 				.addField('`u!lockend`', 'End lockdown of a channel');
-			return message.channel.send({ embeds: [embed2] });
+			return message.channel.send({ embeds: [modEmbed] });
 		}
-
-		if (args[0] === 'info' || 'information') {
-
-			const embed3 = new MessageEmbed()
+		else if (args[0] === 'moderation') {
+			// args = moderation
+			const modEmbed = new MessageEmbed()
+				.setColor(0xFF6663)
+				.setTitle('<:534908853201:894270297723797554> Moderation')
+				.addField('`u!ban`', 'Ban a member.')
+				.addField('`u!kick`', 'Kick a member.')
+				.addField('`u!warn`', 'Warn a member.')
+				.addField('`u!mute`', 'Mute a member so they cannot type or speak.')
+				.addField('`u!purge`', 'Delete a number of messages from a channel.')
+				.addField('`u!lock`', 'Lock a channel with optional timer and message.')
+				.addField('`u!lockend`', 'End lockdown of a channel');
+			return message.channel.send({ embeds: [modEmbed] });
+		}
+		else if (args[0] === 'info') {
+			// args = info
+			const infoEmbed = new MessageEmbed()
 				.setColor(0xFDFD97)
 				.setTitle('<:382067908570:894270287720357948> Information')
 				.addField('`u!ping`', 'Shows the bot\'s ping')
 				.addField('`u!userinfo`', 'Tells you the info of an user')
 				.addField('`u!serverinfo`', 'Tells you the info of a server');
-			return message.channel.send({ embeds: [embed3] });
+			return message.channel.send({ embeds: [infoEmbed] });
 		}
-
-
+		else if (args[0] === 'information') {
+			// args = information
+			const infoEmbed = new MessageEmbed()
+				.setColor(0xFDFD97)
+				.setTitle('<:382067908570:894270287720357948> Information')
+				.addField('`u!ping`', 'Shows the bot\'s ping')
+				.addField('`u!userinfo`', 'Tells you the info of an user')
+				.addField('`u!serverinfo`', 'Tells you the info of a server');
+			return message.channel.send({ embeds: [infoEmbed] });
+		}
 	},
 };
 
